9fed15e7541f5ea4e95ba94a3299362422f0361b
Zain-ul-Abideen <ahmza147@gmail.com>
2023-11-08 21:50:49 +0100

    Collections and Streams

--- CollectionAndStreams.java
+++ CollectionAndStreams.java
@@ -1,23 +1,72 @@
-import java.util.ArrayList;
-import java.util.List;
+import com.sun.xml.internal.bind.v2.runtime.reflect.Lister;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CollectionAndStreams {
-    public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("Alice");
-        names.add("Bob");
-        names.add("Charlie");
-        names.add("David");
-        names.add("Eve");
+    public static void main(String[] args){
 
-        // Filtering elements using a predicate
-        List<String> filteredNames = names.stream().filter(item-> item.startsWith("A")).collect(Collectors.toList());
-        System.out.println("Filtered names: " + filteredNames);
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Zain");
+        arrayList.add("Ahmad");
+        arrayList.add("Hamza");
+        arrayList.add("Fatima");
+        List<String> list= new ArrayList<String>();
+        list = arrayList.stream().map(item->item+" Amjad").collect(Collectors.toList());
+        for(String name : list){
+            System.out.println(name);
+        }
+        System.out.println("\n");
+        list = arrayList.stream().filter(item->item.startsWith("Z")).collect(Collectors.toList());
+        for(String name : list){
+            System.out.println(name);
+        }
+        System.out.println("\n");
+        list = arrayList.stream().map(String::toLowerCase).collect(Collectors.toList());
+        for(String name : list){
+            System.out.println(name);
+        }
+        System.out.println("\n");
 
-        // Transforming elements using a function
+        list = list.stream().map(item->item+" Amjad").collect(Collectors.toList());
+        for(String name : list){
+            System.out.println(name);
+        }
 
-        List<String> uppercaseNames = names.stream().map(String::toUpperCase).collect(Collectors.toList());
-        System.out.println("Uppercase names: " + uppercaseNames);
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("1","Zain");
+        hashMap.put("2","Oain");
+        hashMap.put("3","Pain");
+        hashMap.put("4","Lain");
+        hashMap.put("5","Main");
+        List<String> arrayList1 = new ArrayList<String>();
+        for (Map.Entry<String,String> iterator : hashMap.entrySet()){
+            arrayList1.add(iterator.getValue());
+        }
+        arrayList1 = arrayList1.stream().map(String::toLowerCase).collect(Collectors.toList());
+        System.out.println(arrayList1);
+
+        arrayList1.forEach(System.out::println);
+
+        Optional<String> len =  arrayList1.stream().findAny();
+        System.out.println(len);
+
+        boolean b = arrayList.stream().anyMatch(item->item.equals("zain"));
+        System.out.println(b);
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4);
+        int sum = numbers.stream().reduce(0,(a,c)->a-c);
+        System.out.println(sum);
+
+        ArrayList<Man> men = new ArrayList<Man>();
+        men.add(new Man("zain",24));
+        men.add(new Man("ahmad",23));
+        men.add(new Man("hamza",22));
+        men.add(new Man("fatime",20));
+
+        List<Man> result = men.stream().filter(item->item.getAge().equals(23)).map(item-> new Man("Zain", 99)).collect(Collectors.toList());
+        System.out.println(result);
+
     }
+
 }
--- PractiseZone.java
+++ PractiseZone.java
@@ -6,32 +6,32 @@ import java.util.stream.Collectors;
 public class PractiseZone {
     public static void main(String[] args){
 
-//        ArrayList<String> arrayList = new ArrayList<String>();
-//        arrayList.add("Zain");
-//        arrayList.add("Ahmad");
-//        arrayList.add("Hamza");
-//        arrayList.add("Fatima");
-//        List<String> list= new ArrayList<String>();
-//        list = arrayList.stream().map(item->item+" Amjad").collect(Collectors.toList());
-//        for(String name : list){
-//            System.out.println(name);
-//        }
-//        System.out.println("\n");
-//        list = arrayList.stream().filter(item->item.startsWith("Z")).collect(Collectors.toList());
-//        for(String name : list){
-//            System.out.println(name);
-//        }
-//        System.out.println("\n");
-//        list = arrayList.stream().map(String::toLowerCase).collect(Collectors.toList());
-//        for(String name : list){
-//            System.out.println(name);
-//        }
-//        System.out.println("\n");
-//
-//        list = list.stream().map(item->item+" Amjad").collect(Collectors.toList());
-//        for(String name : list){
-//            System.out.println(name);
-//        }
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Zain");
+        arrayList.add("Ahmad");
+        arrayList.add("Hamza");
+        arrayList.add("Fatima");
+        List<String> list= new ArrayList<String>();
+        list = arrayList.stream().map(item->item+" Amjad").collect(Collectors.toList());
+        for(String name : list){
+            System.out.println(name);
+        }
+        System.out.println("\n");
+        list = arrayList.stream().filter(item->item.startsWith("Z")).collect(Collectors.toList());
+        for(String name : list){
+            System.out.println(name);
+        }
+        System.out.println("\n");
+        list = arrayList.stream().map(String::toLowerCase).collect(Collectors.toList());
+        for(String name : list){
+            System.out.println(name);
+        }
+        System.out.println("\n");
+
+        list = list.stream().map(item->item+" Amjad").collect(Collectors.toList());
+        for(String name : list){
+            System.out.println(name);
+        }
 
         HashMap<String,String> hashMap = new HashMap<>();
         hashMap.put("1","Zain");
@@ -39,16 +39,16 @@ public class PractiseZone {
         hashMap.put("3","Pain");
         hashMap.put("4","Lain");
         hashMap.put("5","Main");
-        List<String> arrayList = new ArrayList<String>();
+        List<String> arrayList1 = new ArrayList<String>();
         for (Map.Entry<String,String> iterator : hashMap.entrySet()){
-            arrayList.add(iterator.getValue());
+            arrayList1.add(iterator.getValue());
         }
-       arrayList = arrayList.stream().map(String::toLowerCase).collect(Collectors.toList());
-       System.out.println(arrayList);
+       arrayList1 = arrayList1.stream().map(String::toLowerCase).collect(Collectors.toList());
+       System.out.println(arrayList1);
 
-       arrayList.forEach(System.out::println);
+       arrayList1.forEach(System.out::println);
 
-      Optional<String> len =  arrayList.stream().findAny();
+      Optional<String> len =  arrayList1.stream().findAny();
       System.out.println(len);
 
       boolean b = arrayList.stream().anyMatch(item->item.equals("zain"));
@@ -57,5 +57,16 @@ public class PractiseZone {
       List<Integer> numbers = Arrays.asList(1,2,3,4);
          int sum = numbers.stream().reduce(0,(a,c)->a-c);
         System.out.println(sum);
+
+        ArrayList<Man> men = new ArrayList<Man>();
+        men.add(new Man("zain",24));
+        men.add(new Man("ahmad",23));
+        men.add(new Man("hamza",22));
+        men.add(new Man("fatime",20));
+
+        List<Man> result = men.stream().filter(item->item.getAge().equals(23)).map(item-> new Man("Zain", 99)).collect(Collectors.toList());
+        System.out.println(result);
+
     }
+
 }
